156d29b72d2b7af75386d072d8bde1acd8e0a1b2
Hector Patiño <hectorpatino24@gmail.com>
2020-09-26 11:56:34 -0500

    modificacion sencilla de travis

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,4 @@
 set -e
 set -x
 
-bash ./scripts/lint.sh
-# Use xdist-pytest --forked to ensure modified sys.path to import relative modules in examples keeps working
-pytest -m unittest
+python -m unittest
