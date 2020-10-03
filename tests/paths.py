b14cff1c7469ba4ce2134f537ebe1c3e442cdd2f
Hector Patiño <hectorpatino24@gmail.com>
2020-10-03 18:44:31 -0500

    añadido sklearn a requiremetns

--- tests/paths.py
+++ tests/paths.py
@@ -1,4 +1,4 @@
-small_2018_dataset = r"data\original\raster\small_datasets\2018.tif"
-small_2010_dataset = r"data\original\raster\small_datasets\2010.tif"
-envelope_2018 = r"data\original\raster\real_datasets\2018\envelope_2018.img"
-coberture_2018_binary = r"data\original\vectorial\reals\2018_cobertures\binary\2018_binary_cobertures.shp"
+small_2018_dataset = r"data/original/raster/small_datasets/2018.tif"
+small_2010_dataset = r"data/original/raster/small_datasets/2010.tif"
+envelope_2018 = r"data/original/raster/real_datasets/2018/envelope_2018.img"
+coberture_2018_binary = r"data/original/vectorial/reals/2018_cobertures/binary/2018_binary_cobertures.shp"
